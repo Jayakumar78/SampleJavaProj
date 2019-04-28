493b0c7d6d9b791e9f84a167fa51d29e37aa4893
jayakumar78 <jayakumar.rathinam@gmail.com>
2019-04-28 19:07:20 +0530

    updated

--- Hello.java
+++ Hello.java
@@ -2,9 +2,9 @@ public class Hello {
 	
 	public static void main(String[] args){
 	
-		for (int i=1; i<=5;i++){
+		for (int i=1; i<=25;i++){
 			System.out.println("Hello World....."+i);
 		}
-	}
+	}git
 		
 }
