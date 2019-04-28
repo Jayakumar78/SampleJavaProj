2eb2674efd005abcbcbbb9a779574b7a1f9030e3
Jayakumar78 <49473011+Jayakumar78@users.noreply.github.com>
2019-04-28 17:36:13 +0530

    Files are added

---
+++ RunFiboProj.bat
@@ -0,0 +1,5 @@
+cd C:\Users\JAYAKUMAR\Desktop\Studies\JavaProj
+
+javac  Fibonacci.java
+
+Java Fibonacci
---
+++ RunHelloProj.bat
@@ -0,0 +1,5 @@
+cd C:\Users\JAYAKUMAR\Desktop\Studies\JavaProj
+
+javac  Hello.java
+
+Java Hello
